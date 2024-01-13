36b0f8c702604de918ac31d9bd05689ba47339d4
Stefano <snebo54@hotmail.com>
2024-01-13 12:19:04 +0100

    solved Input and output spec

--- spec/12_magic_seven_spec.rb
+++ spec/12_magic_seven_spec.rb
@@ -70,34 +70,66 @@ describe MagicSeven do
   # Write a test for each of the following methods:
 
   describe '#subtract_four' do
+    context 'when the previous step is 16' do
+      it 'returns 12' do
+        previous_step = 16
+        result = game.subtract_four(previous_step)
+
+        expect(result).to eq(12)
+      end
+    end
   end
 
   describe '#divide_by_two' do
+    context 'when the previous step clears' do
+      it 'returns 6' do
+        previous_step = 12
+        result = game.divide_by_two(previous_step)
+
+        expect(result).to eq(6)
+      end
+    end
   end
 
   # The following tests will need you to create new instances of MagicSeven with
   # a specific value for the random_number.
   describe '#subtract_random_number' do
+    subject(:game) {described_class.new(24)}
+    context 'checks if works with random number' do
+      it 'returns 21' do
+        result = game.subtract_random_number(3)
+        expect(result).to eq(-21)
+      end
+    end
   end
 
   # The #play method will always return seven! Test this game, using any
   # integer as the random_number. Update the context with the number.
   describe '#play' do
-    context 'when the random number is ...' do
+    context 'when the random number is positive' do
       # remove the 'x' before running this test
-      xit 'will return 7' do
+      subject(:game) {described_class.new(12)}
+      it 'will return 7' do
+        result = game.play
+        expect(result).to eq(7)
       end
     end
 
-    context 'when the random number is ...' do
+    context 'when the random number is a negative' do
       # remove the 'x' before running this test
-      xit 'will return 7' do
+      subject(:game) {described_class.new(-66)}
+      it 'will return 7' do
+        result = game.play
+        expect(result).to eq(7)
       end
     end
 
-    context 'when the random number is ...' do
+    context 'when the random number is a float' do
       # remove the 'x' before running this test
-      xit 'will return 7' do
+      subject(:game) {described_class.new(12.4)}
+      it 'will return 7' do
+        result = game.play.round
+        expect(result).to eq(7)
       end
     end
   end
--- spec/13_input_output_spec.rb
+++ spec/13_input_output_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../lib/13_input_output'
+require 'rspec'
 
 # The file order to complete this lesson:
 # 1. Familarize yourself with the two classes in lib/13_input_output.rb
@@ -28,7 +29,8 @@ require_relative '../lib/13_input_output'
 # context ->  Explains the conditions of the test.
 # it ->       Explains the results of the test.
 
-describe NumberGame do
+RSpec.describe NumberGame do
+  #create a new described class
   subject(:game) { described_class.new }
 
   describe '#initialize' do
@@ -48,10 +50,13 @@ describe NumberGame do
       # Write a similar test to the one above, that uses a custom matcher
       # instead of <, >, =.
       matcher :be_between_zero_and_nine do
+        match { |num| num.between?(0,9) }
       end
 
       # remove the 'x' before running this test
-      xit 'is a number between 0 and 9' do
+      it 'is a number between 0 and 9' do
+        solution = game.solution
+        expect(solution).to be_between_zero_and_nine
       end
     end
   end
@@ -78,8 +83,10 @@ describe NumberGame do
     # Create a new instance of NumberGame and write a test for when the @guess
     # does not equal @solution.
     context 'when user guess is not correct' do
+      subject(:game_end) {described_class.new(2, '5')}
       # remove the 'x' before running this test
-      xit 'is not game over' do
+      it 'is not game over' do
+        expect(game_end).not_to be_game_over
       end
     end
   end
@@ -107,7 +114,10 @@ describe NumberGame do
 
     # Write a test for the following context.
     context 'when given invalid input as argument' do
-      xit 'returns nil' do
+      it 'returns nil' do
+        user_input = 'rand'
+        verified_input = game_check.verify_input(user_input)
+        expect(verified_input).to eq nil
       end
     end
   end
@@ -167,9 +177,15 @@ describe NumberGame do
     # Write a test for the following context.
     context 'when user inputs two incorrect values, then a valid input' do
       before do
+        letter = 'a'
+        symbol = '%'
+        valid_input = '2'
+        allow(game_loop).to receive(:player_input).and_return(letter, symbol, valid_input)
       end
 
-      xit 'completes loop and displays error message twice' do
+      it 'completes loop and displays error message twice' do
+        expect(game_loop).to receive(:puts).with('Input error!').twice
+        game_loop.player_turn
       end
     end
   end
@@ -199,20 +215,25 @@ describe NumberGame do
 
     # Create a new instance of NumberGame, with specific values for @solution,
     # @guess, and @count
+    subject(:game_three) { described_class.new(5, '5', 3) }
     context 'when count is 2-3' do
       # remove the 'x' before running this test
-      xit 'outputs correct phrase' do
-        congrats_phrase = "Congratulations! You picked the random number in 3 guesses!\n"
-        expect { game.final_message }.to output(congrats_phrase).to_stdout
+      it 'outputs correct phrase' do
+        count = game_three.count
+        congrats_phrase = "Congratulations! You picked the random number in #{count} guesses!\n"
+        expect { game_three.final_message }.to output(congrats_phrase).to_stdout
       end
     end
 
     # ASSIGNMENT #6
 
     # Write a test for the following context.
+    subject(:game_4_above) { described_class.new(3, '3', 5) }
     context 'when count is 4 and over' do
       # remove the 'x' before running this test
-      xit 'outputs correct phrase' do
+      it 'outputs correct phrase' do
+        content = "That was hard. It took you 5 guesses!\n"
+        expect {game_4_above.final_message}.to output(content).to_stdout
       end
     end
   end
